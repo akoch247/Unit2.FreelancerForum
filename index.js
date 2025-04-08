bb323eaf5730642541df165422e102b1ee8e2158
Adriana Koch <akoch@oberlin.edu>
2025-04-07 21:00:23 -0400

    add array for name/occupation

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+//Define arrays for possible names and occupations
+const name = ["Alice", "Bob", "Carol"];
+const occupation = ["Writer", "Teacher", "Programmer"];
+const price = ["$30", "$50", "$70" ];
 
